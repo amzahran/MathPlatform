fc1dcb1a8e8c49c8e7cfefb8f7c7669757d23106
amzahran <amzahran@azhar.edu.eg>
2025-06-25 01:12:08 +0300

    Delete Server name.php

--- Server name.php
+++
@@ -1,7 +0,0 @@
-$serverName = "127.0.0.1\\sql"; // استخدم \\ للهروب الصحيح
-$connectionOptions = array(
-    "Database" => "platform",
-    "Uid" => "sa",
-    "PWD" => "123123",
-    "CharacterSet" => "UTF-8"
-);
